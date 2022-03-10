17b538420b751069115ef0a920b10d2d59ba99e3
Keks <keks@htmlacademy.ru>
2022-03-10 18:18:08 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/util.js
@@ -0,0 +1,18 @@
+const getRandomInRange = (min, max) => {
+  let tMin = min;
+  let tMax = max;
+
+  if (max < min) {
+    tMin = max;
+    tMax = min;
+  }
+  return Math.floor(Math.random() * (tMax - tMin + 1)) + tMin;
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInRange(0, elements.length - 1)];
+
+const checkStringLength = (checkString, maxLength) => checkString.length <= maxLength;
+
+checkStringLength(1390, 140);
+
+export{getRandomInRange, getRandomArrayElement};
