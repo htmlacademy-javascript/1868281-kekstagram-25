0b6d135f3c60780c25d99d8121fad254e5c3eaec
nikita hlamov <misa1maho9x2.faires5love@gmail.com>
2022-03-10 19:28:15 +0300

    отрисуй меня полность часть 1 правки

--- js/picture.js
+++ js/picture.js
@@ -1,16 +1,16 @@
-import {createUsersPosts} from './data.js';
-
 const picturesContainer = document.querySelector('.pictures');
 const randomPostTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const usersPosts = createUsersPosts(25);
 const picturesFragment = document.createDocumentFragment();
 
-usersPosts.forEach(({url, likes, comments}) => {
-  const postElement = randomPostTemplate.cloneNode(true);
-  postElement.querySelector('.picture__img').src = url;
-  postElement.querySelector('.picture__likes').textContent = likes;
-  postElement.querySelector('.picture__comments').textContent = comments.length;
-  picturesFragment.append(postElement);
-});
+const renderMiniPosts = (usersPosts) => {
+  usersPosts.forEach(({url, likes, comments}) => {
+    const postElement = randomPostTemplate.cloneNode(true);
+    postElement.querySelector('.picture__img').src = url;
+    postElement.querySelector('.picture__likes').textContent = likes;
+    postElement.querySelector('.picture__comments').textContent = comments.length;
+    picturesFragment.append(postElement);
+  });
+  picturesContainer.append(picturesFragment);
+};
 
-picturesContainer.append(picturesFragment);
+export{renderMiniPosts};
