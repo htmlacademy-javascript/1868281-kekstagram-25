b7262d2cad7ba5b2b5e9034ecef2840c0aee51cc
Keks <keks@htmlacademy.ru>
2022-03-31 19:23:02 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/main.js
+++ 10/js/main.js
@@ -1,10 +1,7 @@
 import {renderMiniPosts} from './picture.js';
-import {setUserFormSubmit} from './user-form.js';
-import {closeUserPhotoUpload} from './user-form.js';
+import './user-form.js';
 import {getData} from './api.js';
 
 getData((data) => {
   renderMiniPosts(data);
 });
-
-setUserFormSubmit(closeUserPhotoUpload);
